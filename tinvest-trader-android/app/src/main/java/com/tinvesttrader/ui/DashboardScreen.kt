c63d3782e7ef0e441906b276b6c3e167ad48fa78
azertydf461-web <azertydf461@gmail.com>
2026-09-19 23:32:36 +0300

    fix: opt in to ExperimentalMaterial3Api for TopAppBar (compileDebugKotlin was failing)

--- tinvest-trader-android/app/src/main/java/com/tinvesttrader/ui/DashboardScreen.kt
+++ tinvest-trader-android/app/src/main/java/com/tinvesttrader/ui/DashboardScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
@@ -24,6 +25,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.tinvesttrader.trading.EngineEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(
     viewModel: TradingViewModel = viewModel(),
